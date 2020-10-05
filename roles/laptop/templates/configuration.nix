1526c3ccebf32123dc4e83f4e47b27a2e74af085
tarnacious <tarn@tarnbarford.net>
2020-10-06 00:28:49 +0200

    Add local samba mount

--- roles/laptop/templates/configuration.nix
+++ roles/laptop/templates/configuration.nix
@@ -12,6 +12,15 @@
     }
   ];
 
+  fileSystems."/mnt/share" = {
+    device = "{{ samba.address }}";
+    fsType = "cifs";
+    options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets" "uid=1000" "gid=100"];
+  };
+
   fileSystems."/mnt/owncloud" = {
     device = "https://owncloud.tarnbarford.net/remote.php/dav/files/tarn/";
     fsType = "davfs";
