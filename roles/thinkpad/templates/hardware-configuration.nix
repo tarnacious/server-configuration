9bcdb0990f20223e809e61efe60a53ec0f030c2d
tarnacious <tarn@tarnbarford.net>
2023-04-18 21:16:04 +0200

    Add swap, 32GB memory does run out

--- roles/thinkpad/templates/hardware-configuration.nix
+++ roles/thinkpad/templates/hardware-configuration.nix
@@ -25,7 +25,12 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 32*1024;
+      }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
