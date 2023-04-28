b47acee616ee7a5cdab33f8eae9488de4df6c7ce
tarnacious <tarn@tarnbarford.net>
2023-04-28 15:37:55 +0200

    Use X11 on thinkpad

--- roles/thinkpad/templates/configuration.nix
+++ roles/thinkpad/templates/configuration.nix
@@ -21,6 +21,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
