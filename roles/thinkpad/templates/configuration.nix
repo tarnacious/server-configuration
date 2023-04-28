7f6ec5436a8a98910c5b09a9a6d3a03c6b5c54cc
tarnacious <tarn@tarnbarford.net>
2023-04-28 15:02:15 +0200

    Group desktop and display settings

--- roles/thinkpad/templates/configuration.nix
+++ roles/thinkpad/templates/configuration.nix
@@ -13,6 +13,19 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "au";
+    enable = true;
+    xkbVariant = "";
+  };
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -88,15 +101,8 @@
   i18n.defaultLocale = "en_AU.utf8";
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = { layout = "au"; xkbVariant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -118,8 +124,6 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tarn = {
