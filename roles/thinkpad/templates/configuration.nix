64846c4a794b6565951252858c57c46fbb4dd694
tarnacious <tarn@tarnbarford.net>
2023-07-12 12:23:00 +0200

    Add some opengl options

--- roles/thinkpad/templates/configuration.nix
+++ roles/thinkpad/templates/configuration.nix
@@ -24,7 +24,11 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
