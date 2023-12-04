269b76f454ac3ad53039df35e937289642cf0707
tarnacious <tarn@tarnbarford.net>
2023-12-04 21:17:48 +0100

    Remove some junk from configuration file

--- roles/thinkpad/templates/configuration.nix
+++ roles/thinkpad/templates/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 { imports = [ # Include the results of the hardware scan.
@@ -29,10 +25,6 @@
     driSupport = true;
     driSupport32Bit = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
 
   specialisation = {
     external-cuda.configuration = {
@@ -281,18 +273,10 @@
 
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions. programs.mtr.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon. services.openssh.enable = true;
-
   networking.firewall = {
     enable = false;
     allowedUDPPorts = [ 51820 ]; # wireguard vpn server
   };
-
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database
