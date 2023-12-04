0795df358a5ae84d77689973c58ab56a6db711ab
tarnacious <tarn@tarnbarford.net>
2023-12-04 21:14:44 +0100

    Auto activate venv environment

--- flake.nix
+++ flake.nix
@@ -13,6 +13,12 @@
           pkgs.python310Packages.virtualenv
           pkgs.python310Packages.lxml
         ];
+        shellHook = ''
+          if [ ! -e "./venv" ]; then
+            python -m venv ./venv
+          fi
+          . ./venv/bin/activate
+        '';
       };
     };
 }
