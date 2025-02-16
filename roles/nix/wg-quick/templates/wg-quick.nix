d4eaa9f921eaba9f252ad9937c77f62d69991afa
tarnacious <tarn@tarnbarford.net>
2025-02-16 23:06:16 +0100

    Wireguard config from thinkpad

--- roles/nix/wg-quick/templates/wg-quick.nix
+++ roles/nix/wg-quick/templates/wg-quick.nix
@@ -17,22 +17,6 @@
       # Path to the private key file.
       privateKeyFile = "/etc/wireguard/keys/private_key";
 
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      postUp = ''
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
-      '';
-
-      # Undo the above
-      preDown = ''
-        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
-      '';
-
       peers = [
         {
           # Public key of the server (not a file path).
