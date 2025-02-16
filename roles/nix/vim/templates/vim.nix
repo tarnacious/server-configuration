1e84621937b1061e19999f7b7ec572051b986751
tarnacious <tarn@tarnbarford.net>
2025-02-16 23:05:10 +0100

    Package updates

--- roles/nix/vim/templates/vim.nix
+++ roles/nix/vim/templates/vim.nix
@@ -19,9 +19,10 @@
             coc-nvim
             coc-tsserver
             coc-java
-            coc-go
             coc-pyright
             coc-prettier
+            pkgs.pkgs-tarn.vimPlugins.vim-ai
+            telescope-nvim
           ];
           opt = [];
         };
